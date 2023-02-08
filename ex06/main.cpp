285983fcc38254a123b883114371a605be420f68
jinholee <jinholee@student.42Seoul.kr>
2023-02-08 16:29:54 +0900

    fix::ex06 harl_filter

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jinholee <jinholee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 12:52:06 by jinholee          #+#    #+#             */
-/*   Updated: 2023/01/27 13:24:50 by jinholee         ###   ########.fr       */
+/*   Updated: 2023/02/08 16:29:34 by jinholee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		Harl::complain(argv[1]);
+	else
+		std::cout << "usage: harl_filter [level, DEBUG|INFO|WARNING|ERROR|other]" << std::endl;
 	return (0);
 }
